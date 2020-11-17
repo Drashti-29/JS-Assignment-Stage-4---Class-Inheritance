5360d2d02df60671c77fa8bc498d7306116f8e99
Drashti <72065999+Drashti-29@users.noreply.github.com>
2020-11-17 18:31:53 +0530

    class

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,20 @@
 // declare class
 
-// export class using module.exports
+// export class using module.exports// declare class
+
+// export class using module.exports//module.exports = Shape;
+// declare class
+
+// export class
+module.exports = class Shape {
+  constructor() {
+    this.color = "red";
+  }
+  drawShape() {
+    console.log("Drawing a shape");
+  }
+  calculateArea() {
+    console.log("don't know area of unknow shape");
+  }
+};
+// export default Shape;
